9d7bb3f2b3b0d359560989fe60788d63f2f87fd0
Spoorti Shetty <spoortishetty2312625@gmail.com>
2023-07-29 14:39:57 +0530

    Iris Flower Classification

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import iris_model
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+
+def basic():
+    if request.method == 'POST':
+        sepal_length = request.form['sepallength']
+        sepal_width = request.form['petalwidth']
+        petal_length = request.form['petallength']
+        petal_width = request.form['petalwidth']
+        y_pred = [[sepal_length,sepal_width,petal_length,petal_width]]
+        trained_model = iris_model.training_model()
+        prediction_value = trained_model.predict(y_pred)
+        setosa = 'The flower is classified as Setosa'
+        versicolor = 'The flower is classified as Versicolor'
+        virginica = 'The flower is classified as Virginica'
+        if prediction_value == 'Iris-setosa':
+           return render_template('index.html', setosa = setosa)
+        elif prediction_value == 'Iris-versicolor':
+            return render_template('index.html', versicolor=versicolor)
+        else:
+            return render_template('index.html', virginica=virginica)
+
+        #return render_template('index.html', prediction_value=prediction_value)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ iris_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+csv_file_path = 'iris.csv'
+data = pd.read_csv(csv_file_path)
+
+X = data.drop(columns=['Species','Id'])  
+y = data['Species']  
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=1,test_size=0.3)
+
+def training_model():
+    model = DecisionTreeClassifier()
+    trained_model = model.fit(X_train, y_train)
+    return trained_model
